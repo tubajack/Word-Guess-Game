eabaf1d3ac040ac0299e8101e94d159530d9f9a3
Jackson Henry <jackson.henry13@yahoo.com>
2019-03-20 14:01:47 -0500

    Initialized win and loss variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,6 @@
 //Display a list of the Simpsons Characters
 var SimpsonsCharacters = ["Homer", "Marge", "Bart", "Lisa", "Maggie", "Grandpa Simpson", "Ralph Wiggum", "Milhouse", "Mr Burns", "Moe"];
+
+
+//Set the counters for number of wins and number of losses
+var wins = 0, losses = 0;
