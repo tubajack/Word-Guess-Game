79f38bfb92f1cbf6da75a4f63f1c0f786c428156
Jackson Henry <jackson.henry13@yahoo.com>
2019-03-22 14:16:55 -0500

    Added new clues for Groundskeeper Willie.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,7 +13,7 @@ var characterClues = ["Catchphrase is 'Doh'",
 "Owner of the Springfield Nuclear Power Plant. Boss of Homer Simpson.",
 "Bartender. Homer Simpson is a frequent visitor to his bar.",
 "Principal of Springfield Elementary School. Has an unhealthy dependence on his mother.",
-
+"Groundskeeper of Springfield Elementary School. Is of Scottish descent. Lives in a shack on school property."
 ];
 
 
